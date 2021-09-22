e9a21486caf44350951ab3bb6e8ec8cdf453374d
Christian <cristian-1823@live.com.mx>
2021-09-22 13:19:16 -0500

    feat: initial commit

---
+++ src/components/Grid/Grid.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Grid = () => {
+    return (
+        <div>
+            Algo
+            <div>
+                Otros
+            </div>
+        </div>
+    )
+}
+
+export default Grid;
---
+++ src/data/courses.ts
@@ -0,0 +1,15 @@
+type course = {
+    title: string;
+    image: string;
+    author: string;
+    rating: number;
+
+    finished: boolean;
+    inProgress: boolean;
+}
+
+const courses = [
+    {
+        title: 'Algo'
+    }
+]
